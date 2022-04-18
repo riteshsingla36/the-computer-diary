8ce2dee467be566bb8c6f4b2760d3e8176fc183b
riteshsingla36 <ritesh.singla36@gmail.com>
2022-04-18 13:12:41 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import Card from './Card';
 import CardQuiz from "./CardQuiz"
-import CardYoutube from "./CardYoutube"
+import CardYoutube from "./CardYoutube";
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <CardQuiz />
-      <CardQuiz />
-      <CardQuiz />
-      <CardQuiz />
-      <CardYoutube />
-      <CardYoutube />
-      <CardYoutube />
-      <CardYoutube />
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <CardQuiz />
+        <CardQuiz />
+        <CardQuiz />
+        <CardQuiz />
+        <CardYoutube />
+        <CardYoutube />
+        <CardYoutube />
+        <CardYoutube />
+      </div>
+    </>
   );
 }
 
